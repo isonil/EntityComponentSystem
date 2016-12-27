1de32e01fc73e81744757ad07ede2fecd577d2be
ison <isonil11@gmail.com>
2016-12-27 21:59:59 +0100

    Event now implements IEquatable<Event>.

--- ECS/Event.cs
+++ ECS/Event.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace ECS
 {
 
-public struct Event
+public struct Event : IEquatable<Event>
 {
     private int kind;
     private System sender;
@@ -25,6 +25,55 @@ public struct Event
         this.sender = sender;
         this.entityID = entityID;
         this.data = data;
+    }
+
+    public static bool operator ==(Event lhs, Event rhs)
+    {
+        return lhs.kind == rhs.kind
+            && lhs.sender == rhs.sender
+            && lhs.entityID == rhs.entityID
+            && lhs.data == rhs.data;
+    }
+
+    public static bool operator !=(Event lhs, Event rhs)
+    {
+        return !(lhs == rhs);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if( !(obj is Event) )
+            return false;
+
+        return Equals((Event)obj);
+    }
+
+    public bool Equals(Event other)
+    {
+        return kind.Equals(other.kind)
+            && System.Equals(sender, other.sender)
+            && entityID.Equals(other.entityID)
+            && object.Equals(data, other.data);
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            int hash = 17;
+
+            hash = hash * 23 + kind;
+
+            if( sender != null )
+                hash = hash * 23 + sender.GetHashCode();
+
+            hash = hash * 23 + entityID;
+
+            if( data != null )
+                hash = hash * 23 + data.GetHashCode();
+
+            return hash;
+        }
     }
 }
 
